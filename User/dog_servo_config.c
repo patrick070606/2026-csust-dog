02e581fd095f644131038e516ad242865b0bfe52
zhou <zhou@users.noreply.local>
2026-08-19 08:06:47 +0800

    恢复该分支对应狗的参数

--- User/dog_servo_config.c
+++ User/dog_servo_config.c
@@ -5,14 +5,14 @@
  * 机械装配后，根据实际腿部位置、转向和限位修改本表。
  */
 static const DogServoConfig_t s_dog_servo_configs[DOG_SERVO_COUNT] = {
-    [DOG_SERVO_LF_KNEE] = {1, -1, 500, -18, 295, 800}, //500 - 200 = 300
-    [DOG_SERVO_LF_HIP]  = {2, -1, 500, -5, 115, 815}, // 435+260 = 695
-    [DOG_SERVO_RF_KNEE] = {3, 1, 500, -4, 100, 685},
-    [DOG_SERVO_RF_HIP]  = {4, 1, 500, -27, 150, 850},
-    [DOG_SERVO_LB_KNEE] = {5, -1, 500, 25, 355, 1000},
-    [DOG_SERVO_LB_HIP]  = {6, -1, 500, -22, 330, 860},
-    [DOG_SERVO_RB_KNEE] = {7, 1, 500, -13, 30, 675},
-    [DOG_SERVO_RB_HIP]  = {8, 1, 500, -11, 70, 625},
+    [DOG_SERVO_LF_KNEE] = {1, -1, 500, -10, 310, 800}, //500 - 200 = 300
+    [DOG_SERVO_LF_HIP]  = {2, -1, 500, -65, 200, 745}, // 435+260 = 695
+    [DOG_SERVO_RF_KNEE] = {3, 1, 500, -20, 200, 630},
+    [DOG_SERVO_RF_HIP]  = {4, 1, 500, -21, 189, 800},
+    [DOG_SERVO_LB_KNEE] = {5, -1, 500, 0, 320, 1000},
+    [DOG_SERVO_LB_HIP]  = {6, -1, 500, 21, 200, 936},
+    [DOG_SERVO_RB_KNEE] = {7, 1, 500, 43, 0, 738},
+    [DOG_SERVO_RB_HIP]  = {8, 1, 500, -40, 40, 800},
 };
 
 const DogServoConfig_t *DogServoConfig_Get(DogServoId_t servo)
